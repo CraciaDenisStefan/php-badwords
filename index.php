ec9ab59ad34d692d9d18c8e26a2cbb7fbc9dc000
CraciDenisStefan <crdenis13@gmail.com>
2023-06-30 16:05:09 +0200

    Esercizio completato

--- index.php
+++ index.php
@@ -6,6 +6,11 @@
     <title>Document</title>
 </head>
 <body>
+    <form action="script.php" method="POST">
+        <input type="text" name="censura" placeholder="parola da censurare">
+        <textarea name="paragrafo" placeholder="inseriesci frase"></textarea>
+        <button type="submit" value="invia">invia</button>
+    </form>
     
 </body>
 </html>
--- script.php
+++ script.php
@@ -1,4 +1,10 @@
 <?php
 
+$parola = $_POST['censura'];
+$frase = $_POST['paragrafo'];
 
+echo "La tua frase :" .$frase. "<br>Lunghezza: " .strlen($frase);
+
+$censuraParola = str_replace($parola, '***', $frase);
+echo "<br>Censura parola: " .$censuraParola. "<br>Lunghezza: " .strlen($frase);
 ?>
